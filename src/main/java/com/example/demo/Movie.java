4f0f91768389fbc0260f70a2583b53d00c8ae078
NonEngineerSalman <oddsalman5@gmail.com>
2025-05-21 08:29:06 +0600

    Salman

---
+++ src/main/java/com/example/demo/Movie.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+public class Movie {
+    private int id;
+    private String title, genre, duration, date, posterPath;
+
+    public Movie(int id, String title, String genre, String duration, String date, String posterPath) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.duration = duration;
+        this.date = date;
+        this.posterPath = posterPath;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+
+    public String getDuration() { return duration; }
+    public void setDuration(String duration) { this.duration = duration; }
+
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+
+    public String getPosterPath() { return posterPath; }
+    public void setPosterPath(String posterPath) { this.posterPath = posterPath; }
+}
